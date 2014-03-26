137cb5bc0e5153ec4d5ce550484dd0bb5931ec5c
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2014-03-26 18:35:15 +0100

    gofmt

--- proxy.go
+++ proxy.go
@@ -1,51 +1,50 @@
 package main
 
 import (
-  "log"
-  "net/http"
-  "time"
   "io"
   "io/ioutil"
-  "strings"
+  "log"
+  "net/http"
   "os"
+  "strings"
+  "time"
 )
 
-  func myHandler(c http.ResponseWriter, req *http.Request) {
+func myHandler(c http.ResponseWriter, req *http.Request) {
 
-    log.Print(req.URL.RequestURI())
+  log.Print(req.URL.RequestURI())
 
-    if (req.URL.RequestURI() == "/") {
+  if req.URL.RequestURI() == "/" {
 
-      http.ServeFile(c, req, "./index.html")
+    http.ServeFile(c, req, "./index.html")
 
-    } else if !(strings.HasPrefix(req.URL.RequestURI(), "/ebauche/Telechargements/ical")) {
+  } else if !(strings.HasPrefix(req.URL.RequestURI(), "/ebauche/Telechargements/ical")) {
 
-      http.Error(c, "Tu veux du pain ?", 403)
+    http.Error(c, "Tu veux du pain ?", 403)
 
-    } else {
+  } else {
 
-      resp, _ := http.Get("http://agenda.ecp.fr" + req.URL.RequestURI())
+    resp, _ := http.Get("http://agenda.ecp.fr" + req.URL.RequestURI())
 
-      defer resp.Body.Close()
-      contents, _ := ioutil.ReadAll(resp.Body)
+    defer resp.Body.Close()
+    contents, _ := ioutil.ReadAll(resp.Body)
 
-      // This is needed for Google Calendar
-      c.Header().Set("Content-Type", "text/calendar; charset=utf-8")
+    // This is needed for Google Calendar
+    c.Header().Set("Content-Type", "text/calendar; charset=utf-8")
 
-      io.WriteString(c, string(contents))
-    }
-
+    io.WriteString(c, string(contents))
   }
+
+}
 
 func main() {
 
-
   s := &http.Server{
-          Addr:           ":"+os.Getenv("PORT"),
-          Handler:        http.HandlerFunc(myHandler),
-          ReadTimeout:    10 * time.Second,
-          WriteTimeout:   10 * time.Second,
-          MaxHeaderBytes: 1 << 20,
+    Addr:           ":" + os.Getenv("PORT"),
+    Handler:        http.HandlerFunc(myHandler),
+    ReadTimeout:    10 * time.Second,
+    WriteTimeout:   10 * time.Second,
+    MaxHeaderBytes: 1 << 20,
   }
   log.Print("listening...")
   log.Fatal(s.ListenAndServe())
